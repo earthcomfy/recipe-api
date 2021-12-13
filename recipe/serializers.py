3a99c104475c0afba153cb308d0b9fe3599a749e
Hannah <66206865+earthcomfy@users.noreply.github.com>
2021-12-13 11:44:28 +0300

    Allow users to create/update a recipe category

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -14,6 +14,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
     username = serializers.SerializerMethodField()
     category_name = serializers.SerializerMethodField()
+    category = RecipeCategorySerializer()
 
     class Meta:
         model = Recipe
@@ -25,3 +26,21 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_category_name(self, obj):
         return obj.category.name
+
+    def create(self, validated_data):
+        category = validated_data.pop('category')
+        category_instance, created = RecipeCategory.objects.get_or_create(
+            **category)
+        recipe_instance = Recipe.objects.create(
+            **validated_data, category=category_instance)
+        return recipe_instance
+
+    def update(self, instance, validated_data):
+        if 'category' in validated_data:
+            nested_serializer = self.fields['category']
+            nested_instance = instance.category
+            nested_data = validated_data.pop('category')
+
+            nested_serializer.update(nested_instance, nested_data)
+
+        return super(RecipeSerializer, self).update(instance, validated_data)
